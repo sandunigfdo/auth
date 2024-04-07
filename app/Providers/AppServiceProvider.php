6ff147a525aed7540d93b3c79e109f2e65966541
Sanduni Fernando <sandunigfdo@gmail.com>
2024-04-07 17:02:42 +1200

    Force https on dev and prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'development' || config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
+        Model::preventSilentlyDiscardingAttributes($this->app->isLocal());
     }
 }
